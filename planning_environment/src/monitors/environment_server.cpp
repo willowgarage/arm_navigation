c03efeeafb165dd6f89422747987cd656f93db3f
sachinc <devnull@localhost>
2011-02-01 18:21:10 +0000

    will not wait if use_collision_map is false

--- planning_environment/src/monitors/environment_server.cpp
+++ planning_environment/src/monitors/environment_server.cpp
@@ -70,7 +70,7 @@ public:
 
     planning_monitor_->waitForState();
     planning_monitor_->startEnvironmentMonitor();
-    while(!planning_monitor_->haveMap()){
+    while(!planning_monitor_->haveMap() && setup_.use_collision_map_){
       ros::Duration().fromSec(0.05).sleep();
     }
 
