d843d138e0c6b951e13b8daad562c217af341614
E. Gil Jones <gjones@willowgarage.com>
2011-07-01 14:28:44 -0700

    Swapped lower and upper bounds for hard limits for prismatic joints

--- planning_models/src/kinematic_model.cpp
+++ planning_models/src/kinematic_model.cpp
@@ -387,7 +387,7 @@ planning_models::KinematicModel::JointModel* planning_models::KinematicModel::co
 	  }
 	else
 	  {
-	    j->setVariableBounds(j->name_, urdf_joint->limits->upper, urdf_joint->limits->lower);
+	    j->setVariableBounds(j->name_, urdf_joint->limits->lower, urdf_joint->limits->upper);
 	  }
 	j->axis_.setValue(urdf_joint->axis.x, urdf_joint->axis.y, urdf_joint->axis.z);
 	result = j;
