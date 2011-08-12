d39b89f4c8a7b4cfe4b3044b929437df4d931d26
Sachin Chitta <sachinc@willowgarage.com>
2011-08-11 21:49:05 -0700

    added simple spline smoother

--- arm_navigation_msgs/include/arm_navigation_msgs/planning_visualizer.h
+++ arm_navigation_msgs/include/arm_navigation_msgs/planning_visualizer.h
@@ -44,9 +44,11 @@ namespace arm_navigation_msgs
 class PlanningVisualizer
 {
 public:
-  PlanningVisualizer(const std::string &topic_name)
+  PlanningVisualizer(const std::string &planning_topic_name,
+                     const std::string &marker_topic_name)
   {
-    display_trajectory_publisher_ = root_handle_.advertise<arm_navigation_msgs::DisplayTrajectory>(topic_name, 1, true);
+    display_trajectory_publisher_ = root_handle_.advertise<arm_navigation_msgs::DisplayTrajectory>(planning_topic_name, 1, true);
+    marker_publisher_ = root_handle_.advertise<arm_navigation_msgs::DisplayTrajectory>(marker_topic_name, 1, true);
   }
 
   void visualizePlan(const trajectory_msgs::JointTrajectory &trajectory,
@@ -58,9 +60,90 @@ public:
     display_trajectory_publisher_.publish(display_trajectory);
   }
 
+  void visualizePlan(const sensor_msgs::JointState &joint_state,
+                     const arm_navigation_msgs::RobotState &robot_state)
+  {
+    arm_navigation_msgs::DisplayTrajectory display_trajectory;
+    display_trajectory.trajectory.joint_trajectory.points.push_back(arm_navigation_msgs::jointStateToJointTrajectoryPoint(joint_state));
+    display_trajectory.robot_state = robot_state;
+    display_trajectory_publisher_.publish(display_trajectory);
+  }
+
+  void visualizeAllowedContactRegions(const std::vector<arm_navigation_msgs::AllowedContactSpecification> &allowed_contacts)
+  {
+    visualization_msgs::MarkerArray mk;
+    mk.markers.resize(allowed_contacts.size());
+    for(unsigned int i=0; i < allowed_contacts.size(); i++) 
+    { 
+      bool valid_shape = true;
+      mk.markers[i].header.stamp = ros::Time::now();
+      mk.markers[i].header.frame_id = allowed_contacts[i].pose_stamped.header.frame_id;
+      mk.markers[i].ns = root_handle_.getNamespace()+allowed_contacts[i].name;
+      mk.markers[i].id = marker_counter_++;
+      if(allowed_contacts[i].shape.type == arm_navigation_msgs::Shape::SPHERE)
+      {        
+        mk.markers[i].type = visualization_msgs::Marker::SPHERE;
+        if(allowed_contacts[i].shape.dimensions.size() >= 1)
+          mk.markers[i].scale.x = mk.markers[i].scale.y = mk.markers[i].scale.z = allowed_contacts[i].shape.dimensions[0];
+        else
+          valid_shape = false;
+      }      
+      else if (allowed_contacts[i].shape.type == arm_navigation_msgs::Shape::BOX)
+      {
+        mk.markers[i].type = visualization_msgs::Marker::CUBE;
+        if(allowed_contacts[i].shape.dimensions.size() >= 3)
+        {
+          mk.markers[i].scale.x = allowed_contacts[i].shape.dimensions[0];
+          mk.markers[i].scale.y = allowed_contacts[i].shape.dimensions[1];
+          mk.markers[i].scale.z = allowed_contacts[i].shape.dimensions[2];
+        }
+        else
+          valid_shape = false;
+      }
+      else if (allowed_contacts[i].shape.type == arm_navigation_msgs::Shape::CYLINDER)
+      {
+        mk.markers[i].type = visualization_msgs::Marker::CYLINDER;
+        if(allowed_contacts[i].shape.dimensions.size() >= 2)
+        {
+          mk.markers[i].scale.x = allowed_contacts[i].shape.dimensions[0];
+          mk.markers[i].scale.y = allowed_contacts[i].shape.dimensions[0];
+          mk.markers[i].scale.z = allowed_contacts[i].shape.dimensions[1];
+        }
+        else
+          valid_shape = false;
+      }
+      else
+      {
+        mk.markers[i].scale.x = mk.markers[i].scale.y = mk.markers[i].scale.z = 0.01;
+        valid_shape = false;
+      }        
+      mk.markers[i].action = visualization_msgs::Marker::ADD;
+      mk.markers[i].pose = allowed_contacts[i].pose_stamped.pose;  
+      if(!valid_shape)
+      {
+        mk.markers[i].scale.x = mk.markers[i].scale.y = mk.markers[i].scale.z = 0.01;
+        mk.markers[i].color.a = 0.3;
+        mk.markers[i].color.r = 1.0;
+        mk.markers[i].color.g = 0.04;
+        mk.markers[i].color.b = 0.04;
+      }
+      else
+      {
+        mk.markers[i].color.a = 1.0;
+        mk.markers[i].color.r = 0.04;
+        mk.markers[i].color.g = 1.0;
+        mk.markers[i].color.b = 0.04;
+      }  
+    }
+    marker_publisher_.publish(mk);
+  }
+
+
 private:
   ros::NodeHandle root_handle_;
-  ros::Publisher display_trajectory_publisher_;
+  ros::Publisher display_trajectory_publisher_, marker_publisher_;
+  unsigned int marker_counter_;
+
 };
 
 }
--- arm_navigation_msgs/include/arm_navigation_msgs/utils.h
+++ arm_navigation_msgs/include/arm_navigation_msgs/utils.h
@@ -14,7 +14,7 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *   * Neither the name of the Willow Garage nor the names of its
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
  * 
@@ -88,4 +88,109 @@ inline void generateDisableAllowedCollisionsWithExclusions(const std::vector<std
     }
   }
 }
+
+bool isPoseGoal(arm_navigation_msgs::GetMotionPlan::Request &req)
+{
+  if (req.motion_plan_request.goal_constraints.joint_constraints.empty() &&         // we have no joint constraints on the goal,
+      req.motion_plan_request.goal_constraints.position_constraints.size() == 1 &&      // we have a single position constraint on the goal
+      req.motion_plan_request.goal_constraints.orientation_constraints.size() ==  1)  // that is active on all 6 DOFs
+    return true;
+  else
+    return false;
+}       
+
+bool hasPoseGoal(arm_navigation_msgs::GetMotionPlan::Request &req)
+{
+  if (req.motion_plan_request.goal_constraints.position_constraints.size() >= 1 &&      // we have a single position constraint on the goal
+      req.motion_plan_request.goal_constraints.orientation_constraints.size() >=  1)  // that is active on all 6 DOFs
+    return true;
+  else
+    return false;
+}       
+
+bool isJointGoal(arm_navigation_msgs::GetMotionPlan::Request &req)
+{
+  if (req.motion_plan_request.goal_constraints.position_constraints.empty() && 
+      req.motion_plan_request.goal_constraints.orientation_constraints.empty() && 
+      !req.motion_plan_request.goal_constraints.joint_constraints.empty())
+    return true;
+  else
+    return false;
+}                   
+
+void discretizeTrajectory(const trajectory_msgs::JointTrajectory &trajectory, 
+                          trajectory_msgs::JointTrajectory &trajectory_out,
+                          const double &trajectory_discretization)
+{    
+  trajectory_out.joint_names = trajectory.joint_names;
+  for(unsigned int i=1; i < trajectory.points.size(); i++)
+  {
+    double diff = 0.0;      
+    for(unsigned int j=0; j < trajectory.points[i].positions.size(); j++)
+    {
+      double start = trajectory.points[i-1].positions[j];
+      double end   = trajectory.points[i].positions[j];
+      if(fabs(end-start) > diff)
+        diff = fabs(end-start);        
+    }
+    int num_intervals =(int) (diff/trajectory_discretization+1.0);
+    
+    for(unsigned int k=0; k < (unsigned int) num_intervals; k++)
+    {
+      trajectory_msgs::JointTrajectoryPoint point;
+      for(unsigned int j=0; j < trajectory.points[i].positions.size(); j++)
+      {
+        double start = trajectory.points[i-1].positions[j];
+        double end   = trajectory.points[i].positions[j];
+        point.positions.push_back(start + (end-start)*k/num_intervals);
+      }
+      point.time_from_start = ros::Duration(trajectory.points[i].time_from_start.toSec() + k* (trajectory.points[i].time_from_start - trajectory.points[i-1].time_from_start).toSec()/num_intervals);
+      trajectory_out.points.push_back(point);
+    }
+  }
+  trajectory_out.points.push_back(trajectory.points.back());
+}
+
+void printConstraints(const arm_navigation_msgs::Constraints &constraints)
+{
+  ROS_DEBUG("Number constraints (Joint,Position, Orientation): %d %d %d");
+  for(unsigned int i=0; i < constraints.joint_constraints.size(); i++)
+    printJointConstraint(constraints.joint_constraint[i],i);
+  for(unsigned int i=0; i < constraints.joint_constraints.size(); i++)
+    printPositionConstraint(constraints.position_constraint[i],i);
+  for(unsigned int i=0; i < constraints.joint_constraints.size(); i++)
+    printOrientationConstraint(constraints.orientation_constraint[i],i);
+}
+
+void printJointConstraint(const arm_navigation_msgs::JointConstraint &constraint, 
+                          const unsigned int &constraint_number)
+{
+  ROS_DEBUG("JC(%d):: %s: desired: %f, range desired: (%f,%f)",(int)constraint_number,constraint.joint_name.c_str(),constraint.position,constraint.position-constraint.tolerance_below,constraint.position+constraint.tolerance_above);
+}
+
+void printPositionConstraint(const arm_navigation_msgs::PositionConstraint &pc, 
+                             const unsigned int &constraint_number)
+{
+  ROS_DEBUG("PC(%d):: %s, frame_id: %s",constraint_number,pc.link_name.c_str(),pc.header.frame_id.c_str());
+  ROS_DEBUG("PC(%d):: Desired position: (%f,%f,%f)",pc.position.x,pc.position.y,pc.position.z);
+  if(pc.constraint_region_shape.type == pc.constraint_region_shape.BOX)
+  {
+    if(pc.constraint_region_shape.dimensions == 3)
+      ROS_DEBUG("PC(%d):: Contraint Region Box Extents (L(x),B(y),H(z)): (%f,%f,%f)",pc.constraint_region_shape.dimensions[0],pc.constraint_region_shape.dimensions[1],pc.constraint_region_shape.dimensions[2]);    
+    ROS_DEBUG("PC(%d):: Constraint Region Orientation: (%f,%f,%f,%f)",pc.constraint_region_orientation.x,pc.constraint_region_orientation.y,pc.constraint_region_orientation.z,pc.constraint_region_orientation.w);    
+  }
+}
+
+void printOrientationConstraint(const arm_navigation_msgs::OrientationConstraint &oc,
+                                const unsigned int &constraint_number)
+{
+  if(oc.type == oc.LINK_FRAME)
+    ROS_DEBUG("OC(%d):: %s, frame_id: %s, type: LINK_FRAME",constraint_number,oc.link_name.c_str(),oc.header.frame_id.c_str());
+  else if(oc.type == oc.HEADER_FRAME)
+    ROS_DEBUG("OC(%d):: %s: frame_id: %s, type: HEADER_FRAME",constraint_number,oc.link_name.c_str(),oc.header.frame_id.c_str());
+  ROS_DEBUG("OC(%d):: Quaternion desired: %f %f %f %f",oc.orientation.x,oc.orientation.y,oc.orientation.z,oc.orientation.w);
+  ROS_DEBUG("OC(%d):: Tolerance(RPY): %f %f %f ",oc.absolute_roll_tolerance,oc.absolute_pitch_tolerance,oc.absolute_yaw_tolerance);
+}
+
+
 }
